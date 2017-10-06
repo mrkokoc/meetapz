c11ababfd913bb0d37c18ae8f6ca83bfbd0e635c
mrkokoc <ilia.guskov@gmail.com>
2017-10-06 18:24:38 +0300

    React Routing & UI

---
+++ client_src/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar/>
+    <div className="container">
+      <Main/>
+    </div>
+    <div className='fixed-action-btn'>
+      <Link to='/meetups/add' className='btn-floating bnt-large waves-effect waves-light red'>
+        <i className='fa fa-plus' aria-hidden='true'></i>
+      </Link>
+    </div>
+  </div>
+);
+
+export default App;
