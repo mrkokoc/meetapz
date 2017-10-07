affaa50d8ecf74f186e632c53fda433e5520512c
mrkokoc <ilia.guskov@gmail.com>
2017-10-07 20:19:18 +0300

    Add, Edit & Delete Meetups

--- client_src/src/components/MeetupDetails.js
+++ client_src/src/components/MeetupDetails.js
@@ -22,8 +22,19 @@ class MeetupDetails extends Component {
     axios.get(`http://localhost:3000/api/meetups/${meetupId}`)
       .then(response => {
         this.setState({details: response.data}, () => {
-          console.log(this.state);
+          // console.log(this.state);
         });
+      })
+      .catch(err => console.log(err));
+  };
+
+  onDelete() {
+
+    let meetupId = this.state.details.id;
+
+    axios.delete(`http://localhost:3000/api/meetups/${meetupId}`)
+      .then(response => {
+        this.props.history.push('/');
       })
       .catch(err => console.log(err));
   };
@@ -43,7 +54,7 @@ class MeetupDetails extends Component {
           </li>
         </ul>
         <Link className='btn' to={`/meetups/edit/${this.state.details.id}`}>Edit</Link>
-        <button className='btn red right'>Delete</button>
+        <button onClick={this.onDelete.bind(this)} className='btn red right'>Delete</button>
       </div>
     );
   };
